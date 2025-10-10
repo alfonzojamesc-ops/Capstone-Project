adf7b41711e2f1dfbac784a353e3501f1b6220cc
ramchriz13 <199692374+ramchriz13@users.noreply.github.com>
2025-10-10 19:30:06 +0800

    working 3dmap added

--- my/component/page/index/3d-map.tsx
+++ my/component/page/index/3d-map.tsx
@@ -12,11 +12,10 @@
 // export default My3DMap;
 
 import modelPath from "@/my/asset/model/parkrealsize.glb";
-import { useGLTF } from "@react-three/drei/native";
+import { OrbitControls, useGLTF } from "@react-three/drei/native";
 import { Canvas } from "@react-three/fiber/native";
 import { Suspense } from "react";
 
-// TODO: type props
 function Model(props: any) {
   const gltf = useGLTF(modelPath);
   return <primitive {...props} object={gltf.scene} />;
@@ -24,11 +23,19 @@ function Model(props: any) {
 
 export default function My3DMap() {
   return (
-    <Canvas>
-      <ambientLight />
+    <Canvas camera={{ position: [0, 50, 200], fov: 45 }}>
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[50, 100, 50]} />
       <Suspense>
-        <Model />
+        <Model scale={0.01} />
       </Suspense>
+      <OrbitControls
+        enableDamping
+        enableZoom
+        minDistance={20}
+        maxDistance={500}
+        zoomSpeed={0.5}
+      />
     </Canvas>
   );
 }
