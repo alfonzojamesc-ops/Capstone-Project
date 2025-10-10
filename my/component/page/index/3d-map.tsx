040a1677c89d669d6e1a8c0006db3bebed2bf7cb
ramchriz13 <199692374+ramchriz13@users.noreply.github.com>
2025-10-10 19:09:19 +0800

    3d map try 1

--- my/component/page/index/3d-map.tsx
+++ my/component/page/index/3d-map.tsx
@@ -1,12 +1,34 @@
-import { Image } from "expo-image";
+// import { Image } from "expo-image";
 
-const My3DMap = () => {
-  // placeholder
+// const My3DMap = () => {
+//   // placeholder
+//   return (
+//     <Image
+//       source={require("@/my/asset/image/my-placeholder.jpg")}
+//       style={{ flex: 1 }}
+//     />
+//   );
+// };
+// export default My3DMap;
+
+import modelPath from "@/my/asset/model/parkrealsize.glb";
+import { useGLTF } from "@react-three/drei/native";
+import { Canvas } from "@react-three/fiber/native";
+import { Suspense } from "react";
+
+// TODO: type props
+function Model(props: any) {
+  const gltf = useGLTF(modelPath);
+  return <primitive {...props} object={gltf.scene} />;
+}
+
+export default function My3DMap() {
   return (
-    <Image
-      source={require("@/my/asset/image/my-placeholder.jpg")}
-      style={{ flex: 1 }}
-    />
+    <Canvas>
+      <ambientLight />
+      <Suspense>
+        <Model />
+      </Suspense>
+    </Canvas>
   );
-};
-export default My3DMap;
+}
