93a09eee5e414e7230b6f63751c4332ceea9bfdf
ramchriz13 <199692374+ramchriz13@users.noreply.github.com>
2025-10-11 21:03:10 +0800

    migrated theme

---
+++ constants/theme.ts
@@ -0,0 +1,39 @@
+const theme = {
+  light: {
+    primary1: "#0073E6",
+    primary2: "#0052A3",
+    primary3: "#C2E7FF",
+    primary4: "#000A14",
+
+    error: "#E63946",
+    success: "#3EB489",
+    warning: "#FA8C16",
+
+    neutral1: "#000305",
+    neutral2: "#333536",
+    neutral3: "#666767",
+    neutral4: "#989a98",
+    neutral5: "#cbccc9",
+    neutral6: "#fefefa",
+  },
+  dark: {
+    primary1: "#0073E6",
+    primary2: "#0052A3",
+    primary3: "#C2E7FF",
+    primary4: "#000A14",
+
+    error: "#E63946",
+    success: "#3EB489",
+    warning: "#FA8C16",
+
+    neutral1: "#fefefa",
+    neutral2: "#cbccc9",
+    neutral3: "#989a98",
+    neutral4: "#666767",
+    neutral5: "#333536",
+    neutral6: "#000305",
+  },
+};
+const { light, dark } = theme;
+export const themeLight = light;
+export const themeDark = dark;
