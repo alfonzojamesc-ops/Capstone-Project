08e4c2e44ab1212f1ce7a7b1bf52e68fa7d04563
ramchriz <199692374+ramchriz13@users.noreply.github.com>
2025-10-21 11:04:25 +0800

    refactor types/firestore-types remove explicit ids

--- types/firestore-types.ts
+++ types/firestore-types.ts
@@ -2,7 +2,7 @@ import { Vector2 } from "@react-three/fiber";
 import { GeoPoint, Timestamp } from "firebase/firestore";
 
 export interface Deceased {
-  id: string; // Firestore document ID
+  // Firestore document ID
   first_name: string;
   middle_name?: string;
   last_name: string;
@@ -17,32 +17,42 @@ export interface Deceased {
 }
 
 export interface Plot {
-  phase: string;
-  block: string;
-  lot: string;
-  id: string; // phase + block + lot
   map_coordinates?: GeoPoint;
-  grid_coordinates?: number[][] | Vector2;
+  grid_coordinates?: Vector2 | number[][];
   status: "available" | "reserved" | "occupied";
-  owner_id?: string; // references PlotOwner.id
+  owner_id?: string;
   maintenance_status?: "good" | "needs_care" | "under_maintenance";
 }
 
+export interface Block {
+  // user-gen docid i.e. Block_1
+  max_plots: number;
+  plots: Plot[]; // user-gen docid i.e. Plot_1
+}
+
+export interface Phase {
+  // user-gen docid i.e. Phase_1A
+  max_blocks: number;
+  blocks: Block[]; // user-gen collectionid i.e. Block_1
+}
+
+// top level collection Phases
+
 export interface PlotOwner {
-  id: string; // Firestore document ID
+  // Firestore document ID
   first_name: string;
   middle_name?: string;
   last_name: string;
   address?: string;
   phone?: string;
   email?: string;
-  purchase_date: Timestamp; 
+  purchase_date: Timestamp;
   deed_number?: string;
   notes?: string;
 }
 
 export interface Admin {
-  id: string; // Firestore document ID
+  // Firestore document ID
   username: string;
   password_hash: string;
   last_login?: Timestamp;
