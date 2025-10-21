97736a4c8e7667cfe95be6265118e09b90b14e10
ramchriz <199692374+ramchriz13@users.noreply.github.com>
2025-10-21 15:07:17 +0800

    partial injecting data to database

--- app/test.tsx
+++ app/test.tsx
@@ -1,102 +1,57 @@
-import { dbDelete, dbRead, dbWrite } from "@/scripts/firestore";
-import React from "react";
-import { Button, View, ViewStyle } from "react-native";
+import { dbAdd } from "@/scripts/firestore";
+import { Phase, Plot } from "@/types/firestore-types";
+import React, { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 export default function Test() {
+  const [hover, setHover] = useState(false);
   return (
-    <View style={containerStyle}>
-      {/* WRITE */}
-      <View style={sectionStyle}>
-        <Button
-          title="Write User1"
-          onPress={() =>
-            dbWrite("users/user1", {
-              name: "Alice",
-              age: 25,
-              profile: { city: "NY", country: "USA" },
-            })
-          }
-        />
-        <Button
-          title="Write User1 Post"
-          onPress={() =>
-            dbWrite("users/user1/posts/post1", {
-              title: "Hello World",
-              content: "This is my first post",
-            })
-          }
-        />
-      </View>
-
-      {/* READ DOCUMENT */}
-      <View style={sectionStyle}>
-        <Button
-          title="Read User1"
-          onPress={async () => {
-            const data = await dbRead("users/user1");
-            console.log("User1 document:", data);
-            console.log("Test document read:", data?.name);
-            console.log("Test document read:", data?.profile.city);
-          }}
-        />
-        <Button
-          title="Read User1 Post1"
-          onPress={async () => {
-            const data = await dbRead("users/user1/posts/post1");
-            console.log("Post1 document:", data);
-          }}
-        />
-      </View>
-
-      {/* READ COLLECTION */}
-      <View style={sectionStyle}>
-        <Button
-          title="Read Users Collection"
-          onPress={async () => {
-            const docs = await dbRead("users");
-            console.log("Users collection:", docs);
-            console.log("Test Collection Read:", docs?.user1.age);
-            console.log("Test Collection Read:", docs?.user1.profile);
-          }}
-        />
-        <Button
-          title="Read User1 Posts Collection"
-          onPress={async () => {
-            const docs = await dbRead("users/user1/posts");
-            console.log("User1 posts collection:", docs);
-          }}
-        />
-      </View>
-
-      {/* DELETE */}
-      <View style={sectionStyle}>
-        <Button
-          title="Delete Post1"
-          onPress={() => dbDelete("users/user1/posts/post1")}
-        />
-        <Button
-          title="Delete User1"
-          onPress={() => dbDelete("users/user1", ["posts"])}
-        />
-        <Button
-          title="Delete Users Collection"
-          onPress={() => dbDelete("users")}
-        />
-      </View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Pressable
+        onHoverIn={() => setHover(true)}
+        onHoverOut={() => setHover(false)}
+        style={{
+          backgroundColor: hover ? "gray" : "black",
+          padding: 20,
+          borderRadius: 10,
+        }}
+        onPress={test}
+      >
+        <Text style={{ color: hover ? "black" : "white" }}>Hello World</Text>
+      </Pressable>
     </View>
   );
 }
 
-const containerStyle: ViewStyle = {
-  flex: 1,
-  justifyContent: "space-evenly",
-  alignItems: "stretch",
-  flexDirection: "row",
-};
+function test() {
+  dbAdd("plots", phases);
+}
 
-const sectionStyle: ViewStyle = {
-  flex: 1,
-  justifyContent: "space-evenly",
-  alignItems: "center",
-  width: "90%",
-};
+const phases: Phase[] = [
+  {
+    status: "available",
+    maintenance_status: "good",
+  },
+  {
+    status: "reserved",
+    owner_id: "owner_001",
+    maintenance_status: "needs_care",
+  },
+  {
+    status: "occupied",
+    owner_id: "owner_002",
+    maintenance_status: "under_maintenance",
+  },
+  {
+    status: "available",
+  },
+  {
+    status: "reserved",
+    owner_id: "owner_003",
+    maintenance_status: "good",
+  },
+  {
+    status: "occupied",
+    owner_id: "owner_004",
+  },
+];
